32b429e438d2f11d49e795a34a72c02c41c2d44d
where-know-return <zzz2920408616@163.com>
2022-03-26 16:30:38 +0800

    ddys

---
+++ 105.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 110;
+
+int n, m;
+int w[N][N];
+int f[N][N];
+
+int main() {
+	int T;
+	cin >> T;
+	while (T -- ) {
+		cin >> n >> m;
+		for (int i = 1; i <= n; i ++ )
+			for (int j = 1; j <= m; j ++ )
+				cin >> w[i][j];
+
+		memset(f, 0, sizeof f);
+		for (int i = 1; i <= n; i ++ )
+			for (int j = 1; j <= m; j ++ )
+				f[i][j] = max(f[i - 1][j], f[i][j - 1]) + w[i][j];
+
+		cout << f[n][m] << endl;
+	}
+
+	return 0;
+}
